081c02588501a3e7c7db836c130bab68db698458
Edward Beazer <ebeazer@thedigitalsages.com>
2023-01-27 14:52:33 -0500

    test

--- vars/nodeTesting.groovy
+++ vars/nodeTesting.groovy
@@ -1,50 +1,25 @@
 #!/usr/bin/env groovy
 
-def call(Closure closure) {
+def call() {
     parallel(
         Linting: {
-            //node {
-stage('Linting') {
-        //    steps {
+            stage('Linting') {
                 bat 'yarn --production=false'
                 bat 'yarn lint'
-                closure()
-           // }
+            }
         }
-          //  }
-        } 
-        // stage('Unit Tests') {
-        //     steps {
-        //         bat 'yarn --production=false'
-        //         bat 'yarn test'
-        //     }
-        // }
-        // stage('Dependency Check') {
-        //     steps {
-        //         bat 'yarn --production=false'
-
-        //         dependencyCheck additionalArguments: '', odcInstallation: '8.0.1', stopBuild: true
-        //         dependencyCheckPublisher failedTotalCritical: 1, failedTotalHigh: 1, unstableTotalLow: 10, unstableTotalMedium: 5
-        //     }
-        // }
+        DependencyCheck: {
+            stage('Dependency Check') {
+                bat 'yarn --production=false'
+                dependencyCheck additionalArguments: '', odcInstallation: '8.0.1', stopBuild: true
+                dependencyCheckPublisher failedTotalCritical: 1, failedTotalHigh: 1, unstableTotalLow: 10, unstableTotalMedium: 5
+            }
+        }
+        UnitTests: {
+            stage('Unit Tests') {
+                bat 'yarn --production=false'
+                bat 'yarn test'
+            }
+        }
     )
 }
-
-// def call(Closure closure) {
-//   parallel({
-//     SanJose: {
-//       node("test && san-jose") {
-//         stage('SanJose') {
-//           closure()
-//         }
-//       } 
-//     },
-//     Dallas: {
-//       node('test dallas') {
-//         stage('Dallas') {
-//           closure()
-//         }
-//       } 
-//     } 
-//   } 
-// }
