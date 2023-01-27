b3ec251223add682aa78785b758d5e932a4e5b42
Edward Beazer <ebeazer@thedigitalsages.com>
2023-01-27 15:50:16 -0500

    test

--- vars/nodeTesting.groovy
+++ vars/nodeTesting.groovy
@@ -4,31 +4,20 @@ def call() {
     parallel(
         Linting: {
             stage('Linting') {
-               // agent any
                 nodejs(nodeJSInstallationName: '16') {
-                    echo pwd()
-                    bat 'yarn --production=false'
                     bat 'yarn lint'
                 }
             }
         },
         'Dependency Check': {
             stage('Dependency Check') {
-                //agent any
-                nodejs(nodeJSInstallationName: '16') {
-                    echo pwd()
-                    bat 'yarn --production=false'
-                    dependencyCheck additionalArguments: '', odcInstallation: '8.0.1', stopBuild: true
-                    dependencyCheckPublisher failedTotalCritical: 1, failedTotalHigh: 1, unstableTotalLow: 10, unstableTotalMedium: 5
-                }
+                dependencyCheck additionalArguments: '', odcInstallation: '8.0.1', stopBuild: true
+                dependencyCheckPublisher failedTotalCritical: 1, failedTotalHigh: 1, unstableTotalLow: 10, unstableTotalMedium: 5
             }
         },
         'Unit Tests': {
             stage('Unit Tests') {
-               // agent any
                 nodejs(nodeJSInstallationName: '16') {
-                    echo pwd()
-                    bat 'yarn --production=false'
                     bat 'yarn test'
                 }
             }
