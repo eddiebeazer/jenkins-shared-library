54d6330305e7b58c04fa8a306da1892fb56fa011
Edward Beazer <ebeazer@thedigitalsages.com>
2023-01-27 14:55:06 -0500

    test

--- vars/nodeTesting.groovy
+++ vars/nodeTesting.groovy
@@ -7,14 +7,14 @@ def call() {
                 bat 'yarn --production=false'
                 bat 'yarn lint'
             }
-        }
+        },
         DependencyCheck: {
             stage('Dependency Check') {
                 bat 'yarn --production=false'
                 dependencyCheck additionalArguments: '', odcInstallation: '8.0.1', stopBuild: true
                 dependencyCheckPublisher failedTotalCritical: 1, failedTotalHigh: 1, unstableTotalLow: 10, unstableTotalMedium: 5
             }
-        }
+        },
         UnitTests: {
             stage('Unit Tests') {
                 bat 'yarn --production=false'
