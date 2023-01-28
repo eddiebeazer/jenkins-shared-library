b28c110999bd98df5101273109947db93fae11ad
Edward Beazer <ebeazer@thedigitalsages.com>
2023-01-27 19:28:29 -0500

    wip

--- vars/golangTesting.groovy
+++ vars/golangTesting.groovy
@@ -4,9 +4,6 @@ def call() {
     parallel(
         'Coverage': {
             stage('Coverage') {
-                echo 'Getting modules'
-                bat 'go get -u -d ./...'
-
                 echo 'Code Coverage'
                 bat 'gocov test ./... | gocov-xml > coverage.xml'
 
@@ -15,18 +12,12 @@ def call() {
         },
         'Dependency Check': {
             stage('Dependency Check') {
-                echo 'Getting modules'
-                bat 'go get -u -d ./...'
-
                 dependencyCheck additionalArguments: '', odcInstallation: '8.0.1', stopBuild: true
                 dependencyCheckPublisher unstableTotalCritical: 1, unstableTotalHigh: 1, unstableTotalLow: 10, unstableTotalMedium: 5
             }
         },
         'Unit Tests': {
             stage('Unit Tests') {
-                echo 'Getting modules'
-                bat 'go get -u -d ./...'
-
                 echo 'JUnit Report'
                 bat 'go test -v 2>&1 ./... | go-junit-report -set-exit-code > report.xml'
 
