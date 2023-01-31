4ff7ea379daf5af8080be7dd1545b4c256cc3ad7
Edward Beazer <ebeazer@thedigitalsages.com>
2023-01-31 11:31:48 -0500

    test

---
+++ vars/nodeFirebasePipeline.groovy
@@ -0,0 +1,85 @@
+#!/usr/bin/env groovy
+
+def call(Map pipelineParams) {
+    pipeline {
+        agent {
+            docker {
+                label 'Docker'
+                image pipelineParams.dockerImage
+            }
+        }
+        stages {
+            stage('Installing Firebase Tools') {
+                steps {
+                    sh 'npm install -g firebase-tools'
+                }
+            }
+            stage('Installing Dependencies') {
+                steps {
+                    sh 'yarn --production=false'
+                }
+            }
+            stage('Linting') {
+                steps {
+                    sh 'yarn lint'
+                }
+            }
+            stage('Dependency Check') {
+                steps {
+                    dependencyCheck additionalArguments: '--disableYarnAudit', odcInstallation: '8.0.1', stopBuild: true
+                    dependencyCheckPublisher unstableTotalCritical: 1, unstableTotalHigh: 1, unstableTotalLow: 10, unstableTotalMedium: 5
+                }
+            }
+            stage('Unit Tests') {
+                steps {
+                    sh 'yarn test'
+                }
+            }
+            stage('Build') {
+                steps {
+                    sh 'yarn build'
+                }
+            }
+            stage('Deploy') {
+                when {
+                    expression {
+                        pipelineParams.deploy == true
+                    }
+                }
+                steps {
+                    script {
+                        withCredentials([
+                            string(credentialsId: 'firebase-ci-token', variable: 'FIREBASE_CI_TOKEN'),
+                            string(credentialsId: 'contentful-access-token', variable: 'CONTENTFUL_ACCESS_TOKEN'),
+                            // string(credentialsId: 'api-token', variable: 'API_TOKEN'), MAYBE
+                            string(credentialsId: 'firebase-api-key', variable: 'FIREBASE_API_KEY'),
+                            string(credentialsId: 'firebase-auth-domain', variable: 'FIREBASE_AUTH_DOMAIN'),
+                            string(credentialsId: 'firebase-project-id', variable: 'FIREBASE_PROJECT_ID'),
+                            string(credentialsId: 'firebase-storage-bucket', variable: 'FIREBASE_STORAGE_BUCKET'),
+                            string(credentialsId: 'firebase-mnessaging-sender-id', variable: 'FIREBASE_MESSAGING_SENDER_ID'),
+                            string(credentialsId: 'firebase-app-id', variable: 'FIREBASE_APP_ID')
+                        ]) {
+                            if (env.BRANCH_NAME == 'main' || env.BRANCH_NAME == 'master') {
+                                sh 'firebase deploy --only hosting:$pipelineParams.projectName --token $FIREBASE_CI_TOKEN'
+                            } else {
+                                sh 'firebase hosting:channel:deploy $BRANCH_NAME --expires 7d --token $FIREBASE_CI_TOKEN'
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+// Use manual tagging for now.  We need to figure out how to get jenkins to download our custom auto tag library
+// stage('Tagging') {
+//     when {
+//         expression {
+//             env.BRANCH_NAME == 'main' || env.BRANCH_NAME == 'master'
+//         }
+//     }
+//     steps {
+//         gitVersioning()
+//     }
+// }
--- vars/nodeTesting.groovy
+++ vars/nodeTesting.groovy
@@ -1,6 +1,26 @@
 #!/usr/bin/env groovy
 
 def call() {
+    stage('Linting') {
+        nodejs(nodeJSInstallationName: '16') {
+            bat 'yarn lint'
+        }
+    }
+    stage('Dependency Check') {
+        nodejs(nodeJSInstallationName: '16') {
+            dependencyCheck additionalArguments: '--disableYarnAudit', odcInstallation: '8.0.1', stopBuild: true
+            dependencyCheckPublisher unstableTotalCritical: 1, unstableTotalHigh: 1, unstableTotalLow: 10, unstableTotalMedium: 5
+        }
+    }
+    stage('Unit Tests') {
+        nodejs(nodeJSInstallationName: '16') {
+            bat 'yarn test'
+        }
+    }
+}
+
+/*
+def call() {
     parallel(
         Linting: {
             stage('Linting') {
@@ -26,3 +46,4 @@ def call() {
         }
     )
 }
+*/
