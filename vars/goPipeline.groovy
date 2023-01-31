0d51fdf7f27777b9752f8db19f49e21f1e7d622e
Edward Beazer <ebeazer@thedigitalsages.com>
2023-01-31 15:29:01 -0500

    test

---
+++ vars/goPipeline.groovy
@@ -0,0 +1,58 @@
+#!/usr/bin/env groovy
+
+def call(Map pipelineParams) {
+    pipeline {
+        agent any
+        environment {
+            PLAYFAB_SECRET     = credentials('playfab-test-secret-key')
+            PLAYFAB_TITLE_ID = credentials('playfab-test-title-id')
+        }
+        tools {
+            go '1.18'
+        }
+        stages {
+            stage('Installing Dependencies') {
+                steps {
+                    sh 'go get -u -d ./...'
+                }
+            }
+            stage('Testing') {
+                stages {
+                    stage('Dependency Check') {
+                        when {
+                            changeRequest()
+                        }
+                        steps {
+                            dependencyCheck additionalArguments: '', odcInstallation: '8.0.1', stopBuild: true
+                            dependencyCheckPublisher failedTotalCritical: 1, failedTotalHigh: 1, unstableTotalLow: 10, unstableTotalMedium: 5
+                        }
+                    }
+                    stage('Code Coverage') {
+                        steps {
+                            sh 'gocov test ./... | gocov-xml > coverage.xml'
+
+                            publishCoverage adapters: [cobertura('coverage.xml')]
+                        }
+                    }
+                    stage('Unit Tests') {
+                        steps {
+                            sh 'go test -v 2>&1 ./... | go-junit-report -set-exit-code > report.xml'
+
+                            junit testResults: 'report.xml', skipPublishingChecks: false
+                        }
+                    }
+                }
+            }
+            stage('Build') {
+                when {
+                    expression {
+                        pipelineParams.deploy == true
+                    }
+                }
+                steps {
+                    echo 'building'
+                }
+            }
+        }
+    }
+}
